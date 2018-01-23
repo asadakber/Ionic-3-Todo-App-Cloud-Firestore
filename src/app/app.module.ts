faa1d4d7642e569d501aee03a7cbb4a4c03f4707
asadakber <asad.akberweb@gmail.com>
2018-01-23 17:45:20 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FormsModule } from '@angular/forms';
+export const config = {
+  apiKey: "AIzaSyA47KGEZ3vInf1HC7XDK4FAZ2t0US4LuhI",
+  authDomain: "cloud-firestore-32c5d.firebaseapp.com",
+  databaseURL: "https://cloud-firestore-32c5d.firebaseio.com",
+  projectId: "cloud-firestore-32c5d",
+  storageBucket: "cloud-firestore-32c5d.appspot.com",
+  messagingSenderId: "864664871265"
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +24,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    FormsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Todo {
+  name: string
+}
+
+interface todoId extends Todo {
+  id: string;
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  todocol: AngularFirestoreCollection<Todo>;
+  // todos: Observable<Todo[]>;
+  todos: any;
+  name: string;
+  tododoc: AngularFirestoreDocument<Todo>;
+  todoes: Observable<Todo>;
+  constructor(private afs: AngularFirestore,public navCtrl: NavController) {
+    this.todocol = this.afs.collection('todo');
+   // this.todos = this.todocol.valueChanges();
+   this.todos = this.todocol.snapshotChanges()
+   .map(actions => {
+     return actions.map(a => {
+       const data = a.payload.doc.data() as Todo;
+       const id = a.payload.doc.id;
+       return { id, data };
+     });
+   });
+  }
 
-  constructor(public navCtrl: NavController) {
+  addTodo() {
+    this.afs.collection('todo').add({'name': this.name})
+  }
 
+  deleteTodo(todoId) {
+    this.afs.doc('todo/' + todoId).delete();
   }
 
 }
